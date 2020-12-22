5aa607f0bb92eb5aa31d2def466d784ada227708
mocha <syda07@gmail.com>
2020-12-23 05:17:50 +0900

    수정: md5 암호화

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
+const md5 = require('md5');
 
 const app = express();
 
@@ -17,7 +17,6 @@ const userSchema = new mongoose.Schema({
   password: String
 })
 
-userSchema.plugin(encrypt, {secret: process.env.secret, encryptedFields: ['password']});
 const User = new mongoose.model('User', userSchema);
 
 app.use(express.static('public'));
@@ -41,7 +40,7 @@ app.get('/register', (req,res) => {
 app.post('/register', (req, res) => {
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   });
 
   newUser.save(err => {
@@ -52,7 +51,7 @@ app.post('/register', (req, res) => {
 
 app.post('/login', (req, res) => {
   const username= req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
 
   User.findOne({email: username}, (err, foundUser) => {
     if (err){
