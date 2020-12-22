2d54604c37fd1a590c85e3b342b18ffdbe84ee3b
mocha <syda07@gmail.com>
2020-12-22 22:25:23 +0900

    추가: 기본셋팅

--- app.js
+++ app.js
@@ -1,16 +1,54 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.set('useUnifiedTopology', true );
+mongoose.set('useNewUrlParser', true );
+
+mongoose.connect('mongodb://localhost:27017/userDB');
+
+
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String
+})
+
+const User = new mongoose.model('User', userSchema);
 
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', (req,res) => {
-  res.send('hello world')
+  res.render('home')
 })
+
+
+app.get('/login', (req,res) => {
+  res.render('login')
+})
+
+
+app.get('/register', (req,res) => {
+  res.render('register')
+})
+
+
+app.post('/register', (req, res) => {
+  const newUser = new User({
+    email: req.body.userName;
+  });
+
+  newUser.save(err => {
+    err ? console.log(err) : res.render('secrets');
+  })
+})
+
+
+
 
 app.listen(3000, ()=>{
   console.log('server stated on port 3000');
