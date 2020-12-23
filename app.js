14a41501821e79ac80d62efc77323523982dbc0e
mocha <syda07@gmail.com>
2020-12-24 00:05:03 +0900

    변경: md5 👉 salt

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const md5 = require('md5');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const someOtherPlaintextPassword = 'not_bacon';
 
 const app = express();
 
@@ -38,29 +40,34 @@ app.get('/register', (req,res) => {
 
 
 app.post('/register', (req, res) => {
-  const newUser = new User({
-    email: req.body.username,
-    password: md5(req.body.password)
-  });
 
-  newUser.save(err => {
-    err ? console.log(err) : res.render('secrets')
-  })
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    const newUser = new User({
+      email: req.body.username,
+      password: hash
+    });
+
+    newUser.save(err => {
+      err ? console.log(err) : res.render('secrets')
+    })
+  });
 })
 
 
 app.post('/login', (req, res) => {
   const username= req.body.username;
-  const password = md5(req.body.password);
+  const password = req.body.password;
 
   User.findOne({email: username}, (err, foundUser) => {
     if (err){
       console.log(err);
     } else {
       if (foundUser){
-        if (foundUser.password === password){
-          res.render('secrets')
-        }
+        bcrypt.compare(password, foundUser.password, (err, result) => {
+          if (result === true){
+            res.render('secrets');
+          }
+        });
       }
     }
   })
@@ -68,5 +75,5 @@ app.post('/login', (req, res) => {
 
 
 app.listen(3000, ()=>{
-  console.log('server stated on port 3000');
+  console.log('server started on port 3000');
 })
